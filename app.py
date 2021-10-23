1b747a9b13620b854c666d669e0785f13f789a9e
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2021-10-23 23:55:12 +0300

    Add missing `html` argument for mounting

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="site"))
+app.mount("/", StaticFiles(directory="site", html=True))
